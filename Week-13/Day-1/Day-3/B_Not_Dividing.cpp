0997bd8dca341c29cbcff4f083529c45bc58457d
Mahajabin Akter Ritu <159685172+Mahajabin217@users.noreply.github.com>
2024-06-24 20:13:54 +0600

    Add files via upload
    Week-13 Day-3 codes

---
+++ Week-13/Day-1/Day-3/B_Not_Dividing.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define lb "\n"
+#define ll long long
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void solve();
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // Writer    :  Mahajabin  Akter Ritu
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] == 1)
+        {
+            a[i]++;
+        }
+    }
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] % a[i - 1] == 0)
+        {
+            a[i]++;
+        }
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << lb;
+}
---
+++ Week-13/Day-1/Day-3/B_Vaccination.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define lb "\n"
+#define ll long long
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void solve();
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // Writer    :  Mahajabin  Akter Ritu
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    ll n, k, d, w;
+    cin >> n >> k >> d >> w;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    ll cnt = 0;
+    ll val = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (i == 0 || cnt < a[i] || val < 1)
+        {
+            ans++;
+            val = k;
+            cnt = a[i] + w + d;
+        }
+        val--;
+    }
+    cout << ans << lb;
+}
