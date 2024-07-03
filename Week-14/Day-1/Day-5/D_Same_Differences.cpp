3832ffb3ab66472065a187bb052511d705d6473d
Mahajabin Akter Ritu <159685172+Mahajabin217@users.noreply.github.com>
2024-07-03 23:03:17 +0600

    Add files via upload
    Week-14 Day-5 codes

---
+++ Week-14/Day-1/Day-5/D_Same_Differences.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define lb "\n"
+#define ll long long
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void solve();
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // Writer    :  Mahajabin  Akter Ritu
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    map<ll, ll> mp;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll a;
+        cin >> a;
+        ans += mp[a - i];
+        mp[a - i]++;
+    }
+    cout << ans << lb;
+}
