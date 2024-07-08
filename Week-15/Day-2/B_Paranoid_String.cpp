7c1073c60b5e7068ca35b74310b599b2ce5bff92
Mahajabin Akter Ritu <159685172+Mahajabin217@users.noreply.github.com>
2024-07-08 18:55:46 +0600

    Add files via upload
    Week-15 Day-2 codes

---
+++ Week-15/Day-2/B_Paranoid_String.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define lb "\n"
+#define ll long long
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void solve();
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // Writer    :  Mahajabin  Akter Ritu
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll ans = n;
+    for (ll i = 1; i < n; i++)
+    {
+        if (s[i] != s[i - 1])
+        {
+            ans += i;
+        }
+    }
+    cout << ans << lb;
+}
